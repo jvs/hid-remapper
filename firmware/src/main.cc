61624f936eb50e0db4bb3def354b6fb448553722
jvs <vonseg@protonmail.com>
2025-08-14 12:28:19 -0500

    Try removing another persist config function.

--- firmware/src/main.cc
+++ firmware/src/main.cc
@@ -134,18 +134,6 @@ void write_gpio() {
     memset(gpio_out_state, 0, sizeof(gpio_out_state));
 }
 
-
-void do_persist_config(uint8_t* buffer) {
-#if !PICO_COPY_TO_RAM
-    uint32_t ints = save_and_disable_interrupts();
-#endif
-    flash_range_erase(CONFIG_OFFSET_IN_FLASH, PERSISTED_CONFIG_SIZE);
-    flash_range_program(CONFIG_OFFSET_IN_FLASH, buffer, PERSISTED_CONFIG_SIZE);
-#if !PICO_COPY_TO_RAM
-    restore_interrupts(ints);
-#endif
-}
-
 void reset_to_bootloader() {
     reset_usb_boot(0, 0);
 }
--- firmware/src/platform.h
+++ firmware/src/platform.h
@@ -4,8 +4,6 @@
 #include <stdint.h>
 #include <types.h>
 
-void do_persist_config(uint8_t* buffer);
-
 void reset_to_bootloader();
 void pair_new_device();
 void clear_bonds();
