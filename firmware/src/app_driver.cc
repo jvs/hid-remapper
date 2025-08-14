0897c42c873e360aa7fc0b53290cfde2cff9e7b9
John Von Seggern <vonseg@protonmail.com>
2025-08-14 08:51:09 -0500

    Remove midi support.

--- firmware/src/app_driver.cc
+++ firmware/src/app_driver.cc
@@ -1,18 +1,4 @@
-#include "usb_midi_host.h"
-
 usbh_class_driver_t const* usbh_app_driver_get_cb(uint8_t* driver_count) {
-    static usbh_class_driver_t host_driver[] = {
-        {
-#if CFG_TUSB_DEBUG >= 2
-            .name = "MIDIH",
-#endif
-            .init = midih_init,
-            .open = midih_open,
-            .set_config = midih_set_config,
-            .xfer_cb = midih_xfer_cb,
-            .close = midih_close,
-        },
-    };
-    *driver_count = 1;
-    return host_driver;
+    *driver_count = 0;
+    return NULL;
 }
